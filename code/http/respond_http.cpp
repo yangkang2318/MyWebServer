e26d389b7cb531aa52c72e498bad889e142d746b
yangkang <1252317801@qq.com>
2024-01-12 13:48:18 +0800

    first commit

--- code/http/respond_http.cpp
+++ code/http/respond_http.cpp
@@ -146,7 +146,7 @@ void HttpResponse::WriteErrorContent(Buffer &buff, string message) {
     }
     body += to_string(m_code) + " : " + status + "\n";
     body += "<p>" + message + "</p>";
-    body += "<hr><em>TinyWebServer</em></body></html>";
+    body += "<hr><em>MyWebServer</em></body></html>";
 
     buff.Append("Content-length: " + to_string(body.size()) + "\r\n\r\n");
     buff.Append(body);
